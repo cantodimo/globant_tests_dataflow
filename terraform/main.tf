a114176a555ef749bc3d3ce5cd7a8d5776406548
cantodimo <59267515+cantodimo@users.noreply.github.com>
2023-08-08 14:24:54 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,7 +37,7 @@ resource "google_dataflow_flex_template_job" "ejecutar_job" {
   region                = local.region
   max_workers           = 1
   temp_gcs_location     = "gs://dataflow-staging-us-west4-760721552379/temp"
-  template_gcs_path     = "gs://${local.template_bucket}/gitlab_test/itd-saptm-apachebeam/streaming.json"
+  container_spec_gcs_path     = "gs://${local.template_bucket}/gitlab_test/itd-saptm-apachebeam/streaming.json"
   network               = "default"
   parameters = {
     bootstrap_servers = "35.193.114.205:9092"
